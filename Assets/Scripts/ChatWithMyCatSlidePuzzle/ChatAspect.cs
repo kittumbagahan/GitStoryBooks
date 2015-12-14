d03a5ff92bf2b8bf5bc064ac33e9a6e275c78cd2
DAC <kittumbagahan@gmail.com>
2015-12-14 14:40:32 +0800

    Chat cat puzzle
    Chat with my cat slide puzzle

---
+++ Assets/Scripts/ChatWithMyCatSlidePuzzle/ChatAspect.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChatAspect : AspectManager {
+
+	[SerializeField]
+	RectTransform gameCompleteDialog;
+
+	// Use this for initialization
+	void Start () {
+		Aspect();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GameCompleteDialog()
+	{
+		StartCoroutine("_IEGameComplete");
+	}
+
+	IEnumerator _IEGameComplete()
+	{
+		gameCompleteDialog.gameObject.SetActive(true);
+		yield return new WaitForSeconds(3);
+		gameCompleteDialog.gameObject.SetActive(false);
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
